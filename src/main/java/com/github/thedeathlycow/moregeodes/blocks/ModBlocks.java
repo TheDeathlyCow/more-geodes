677b425b42fd0dd23e98a15467cbc1e02f9458a0
SalveMundiProd <salvemundiproductions@gmail.com>
2021-10-20 02:55:37 -0400

    commented out experimental quartz geode; correctly configured for client and server initialization in newer versions of fabric; updated emerald geode spawn location for UNDERGROUND biomes for compatibility with 1.18 datapack and 1.18

--- src/main/java/com/github/thedeathlycow/moregeodes/blocks/ModBlocks.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/blocks/ModBlocks.java
@@ -1,6 +1,6 @@
 package com.github.thedeathlycow.moregeodes.blocks;
 
-import com.github.thedeathlycow.moregeodes.MoreGeodes;
+import com.github.thedeathlycow.moregeodes.MoreGeodesDedicatedServer;
 import com.google.common.collect.ImmutableList;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -21,22 +21,22 @@ public class ModBlocks {
     public static AmethystClusterBlock MEDIUM_EMERALD_BUD = new AmethystClusterBlock(4, 3, FabricBlockSettings.of(Material.AMETHYST, MapColor.EMERALD_GREEN).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.AMETHYST_CLUSTER).strength(1.5f).luminance((blockState) -> 2));
     public static AmethystClusterBlock SMALL_EMERALD_BUD = new AmethystClusterBlock(3, 4, FabricBlockSettings.of(Material.AMETHYST, MapColor.EMERALD_GREEN).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.AMETHYST_CLUSTER).strength(1.5f).luminance((blockState) -> 1));
 
-    public static Block QUARTZ_GEODE_BLOCK = new AmethystBlock(FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).strength(1.5F).sounds(BlockSoundGroup.AMETHYST_BLOCK).breakByTool(FabricToolTags.PICKAXES).requiresTool());
+    //public static Block QUARTZ_GEODE_BLOCK = new AmethystBlock(FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).strength(1.5F).sounds(BlockSoundGroup.AMETHYST_BLOCK).breakByTool(FabricToolTags.PICKAXES).requiresTool());
 
-    public static AmethystClusterBlock QUARTZ_CLUSTER = new AmethystClusterBlock(7, 3, FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.ANCIENT_DEBRIS).strength(1.5f).luminance((blockState) -> 5));
-    public static AmethystClusterBlock LARGE_QUARTZ_BUD = new AmethystClusterBlock(5, 3, FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.NETHER_GOLD_ORE).strength(1.5f).luminance((blockState) -> 5));
-    public static AmethystClusterBlock MEDIUM_QUARTZ_BUD = new AmethystClusterBlock(4, 3, FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.NETHER_GOLD_ORE).strength(1.5f).luminance((blockState) -> 5));
-    public static AmethystClusterBlock SMALL_QUARTZ_BUD = new AmethystClusterBlock(3, 4, FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.NETHER_GOLD_ORE).strength(1.5f).luminance((blockState) -> 5));
+    //public static AmethystClusterBlock QUARTZ_CLUSTER = new AmethystClusterBlock(7, 3, FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.ANCIENT_DEBRIS).strength(1.5f).luminance((blockState) -> 5));
+    //public static AmethystClusterBlock LARGE_QUARTZ_BUD = new AmethystClusterBlock(5, 3, FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.NETHER_GOLD_ORE).strength(1.5f).luminance((blockState) -> 5));
+    //public static AmethystClusterBlock MEDIUM_QUARTZ_BUD = new AmethystClusterBlock(4, 3, FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.NETHER_GOLD_ORE).strength(1.5f).luminance((blockState) -> 5));
+    //public static AmethystClusterBlock SMALL_QUARTZ_BUD = new AmethystClusterBlock(3, 4, FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).nonOpaque().breakByTool(FabricToolTags.PICKAXES).requiresTool().ticksRandomly().sounds(BlockSoundGroup.NETHER_GOLD_ORE).strength(1.5f).luminance((blockState) -> 5));
 
     public static CustomBuddingBlock BUDDING_EMERALD = new CustomBuddingBlock(
             FabricBlockSettings.of(Material.AMETHYST, MapColor.EMERALD_GREEN).ticksRandomly().strength(1.5F).sounds(BlockSoundGroup.AMETHYST_BLOCK).breakByTool(FabricToolTags.PICKAXES).requiresTool(),
             ImmutableList.of(SMALL_EMERALD_BUD, MEDIUM_EMERALD_BUD, LARGE_EMERALD_BUD, EMERALD_CLUSTER)
     );
 
-    public static CustomBuddingBlock BUDDING_QUARTZ = new CustomBuddingBlock(
-            FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).ticksRandomly().strength(1.5f).sounds(BlockSoundGroup.AMETHYST_BLOCK).breakByTool(FabricToolTags.PICKAXES).requiresTool(),
-            ImmutableList.of(SMALL_QUARTZ_BUD, MEDIUM_QUARTZ_BUD, LARGE_QUARTZ_BUD, QUARTZ_CLUSTER)
-    );
+//    public static CustomBuddingBlock BUDDING_QUARTZ = new CustomBuddingBlock(
+//            FabricBlockSettings.of(Material.AMETHYST, MapColor.IRON_GRAY).ticksRandomly().strength(1.5f).sounds(BlockSoundGroup.AMETHYST_BLOCK).breakByTool(FabricToolTags.PICKAXES).requiresTool(),
+//            ImmutableList.of(SMALL_QUARTZ_BUD, MEDIUM_QUARTZ_BUD, LARGE_QUARTZ_BUD, QUARTZ_CLUSTER)
+//    );
 
     public static void registerBlocks() {
         register("emerald_geode", EMERALD_GEODE);
@@ -45,11 +45,11 @@ public class ModBlocks {
         register("large_emerald_bud", LARGE_EMERALD_BUD);
         register("medium_emerald_bud", MEDIUM_EMERALD_BUD);
         register("small_emerald_bud", SMALL_EMERALD_BUD);
-        register("quartz_geode_block", QUARTZ_GEODE_BLOCK);
-        register("budding_quartz", BUDDING_QUARTZ);
-        register("large_quartz_bud", LARGE_QUARTZ_BUD);
-        register("medium_quartz_bud", MEDIUM_QUARTZ_BUD);
-        register("small_quartz_bud", SMALL_QUARTZ_BUD);
+//        register("quartz_geode_block", QUARTZ_GEODE_BLOCK);
+//        register("budding_quartz", BUDDING_QUARTZ);
+//        register("large_quartz_bud", LARGE_QUARTZ_BUD);
+//        register("medium_quartz_bud", MEDIUM_QUARTZ_BUD);
+//        register("small_quartz_bud", SMALL_QUARTZ_BUD);
     }
 
     public static void registerCutouts() {
@@ -60,7 +60,7 @@ public class ModBlocks {
     }
 
     private static void register(String name, Block block) {
-        Registry.register(Registry.BLOCK, new Identifier(MoreGeodes.MODID, name), block);
+        Registry.register(Registry.BLOCK, new Identifier(MoreGeodesDedicatedServer.MODID, name), block);
     }
 
 }
--- src/main/java/com/github/thedeathlycow/moregeodes/features/ModFeatures.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/features/ModFeatures.java
@@ -1,11 +1,11 @@
 package com.github.thedeathlycow.moregeodes.features;
 
-import com.github.thedeathlycow.moregeodes.MoreGeodes;
+import com.github.thedeathlycow.moregeodes.MoreGeodesDedicatedServer;
 import com.github.thedeathlycow.moregeodes.blocks.CustomGeode;
 import com.github.thedeathlycow.moregeodes.blocks.ModBlocks;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.block.Blocks;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
@@ -21,31 +21,31 @@ public class ModFeatures {
             ModBlocks.EMERALD_GEODE
     );
 
-    public static CustomGeode QUARTZ_GEODE = new CustomGeode(
-            ModBlocks.BUDDING_QUARTZ,
-            ModBlocks.QUARTZ_GEODE_BLOCK
-    );
+//    public static CustomGeode QUARTZ_GEODE = new CustomGeode(
+//            ModBlocks.BUDDING_QUARTZ,
+//            ModBlocks.QUARTZ_GEODE_BLOCK
+//    );
 
     public static void registerFeatures() {
         register("emerald_geode", EMERALD_GEODE);
-        RegistryKey<ConfiguredFeature<?, ?>> emeraldGeode = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier(MoreGeodes.MODID, "emerald_geode"));
+        RegistryKey<ConfiguredFeature<?, ?>> emeraldGeode = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier(MoreGeodesDedicatedServer.MODID, "emerald_geode"));
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, emeraldGeode.getValue(), ModConfiguredFeatures.EMERALD_GEODE);
 
-        register("quartz_geode", QUARTZ_GEODE);
-        RegistryKey<ConfiguredFeature<?, ?>> quartzGeode = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier(MoreGeodes.MODID, "quartz_geode"));
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, quartzGeode.getValue(), ModConfiguredFeatures.QUARTZ_GEODE);
+//        register("quartz_geode", QUARTZ_GEODE);
+//        RegistryKey<ConfiguredFeature<?, ?>> quartzGeode = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier(MoreGeodesDedicatedServer.MODID, "quartz_geode"));
+//        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, quartzGeode.getValue(), ModConfiguredFeatures.QUARTZ_GEODE);
 
         BiomeModifications.addFeature(
-                BiomeSelectors.categories(Biome.Category.EXTREME_HILLS),
+                BiomeSelectors.categories(Biome.Category.UNDERGROUND),
                 GenerationStep.Feature.UNDERGROUND_ORES,
                 emeraldGeode
         );
 
-        BiomeModifications.addFeature(
-                BiomeSelectors.categories(Biome.Category.NETHER),
-                GenerationStep.Feature.UNDERGROUND_ORES,
-                quartzGeode
-        );
+//        BiomeModifications.addFeature(
+//                BiomeSelectors.categories(Biome.Category.NETHER),
+//                GenerationStep.Feature.UNDERGROUND_ORES,
+//                quartzGeode
+//        );
     }
 
 
