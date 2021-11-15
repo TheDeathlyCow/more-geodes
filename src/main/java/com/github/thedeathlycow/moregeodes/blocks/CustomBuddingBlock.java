a9213f35a194f4e40cc975a5ee1eb17fe2301111
Michael Alpenfels <mwa172@uclive.ac.nz>
2021-11-16 00:49:37 +1300

    budding block grow fix

--- src/main/java/com/github/thedeathlycow/moregeodes/blocks/CustomBuddingBlock.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/blocks/CustomBuddingBlock.java
@@ -16,17 +16,17 @@ public class CustomBuddingBlock extends Block {
 
     private static final Direction[] DIRECTIONS = Direction.values();
 
-    private final Block SMALL_CLUSTER;
-    private final Block MEDIUM_CLUSTER;
-    private final Block LARGE_CLUSTER;
+    private final Block SMALL_BUD;
+    private final Block MEDIUM_BUD;
+    private final Block LARGE_BUD;
     private final Block FULL_BUD;
 
     public CustomBuddingBlock(AbstractBlock.Settings settings, List<AmethystClusterBlock> clusters) {
         super(settings);
         assert clusters.size() == 4;
-        this.SMALL_CLUSTER = clusters.get(0);
-        this.MEDIUM_CLUSTER = clusters.get(1);
-        this.LARGE_CLUSTER = clusters.get(2);
+        this.SMALL_BUD = clusters.get(0);
+        this.MEDIUM_BUD = clusters.get(1);
+        this.LARGE_BUD = clusters.get(2);
         this.FULL_BUD = clusters.get(3);
     }
 
@@ -42,25 +42,25 @@ public class CustomBuddingBlock extends Block {
             BlockState blockState = world.getBlockState(blockPos);
             Block block = null;
             if (canGrowIn(blockState)) {
-                block = this.SMALL_CLUSTER;
-            } else if (blockState.isOf(ModBlocks.SMALL_EMERALD_BUD) && blockState.get(AmethystClusterBlock.FACING) == direction) {
-                block = this.MEDIUM_CLUSTER;
-            } else if (blockState.isOf(ModBlocks.MEDIUM_EMERALD_BUD) && blockState.get(AmethystClusterBlock.FACING) == direction) {
-                block = this.LARGE_CLUSTER;
-            } else if (blockState.isOf(ModBlocks.LARGE_EMERALD_BUD) && blockState.get(AmethystClusterBlock.FACING) == direction) {
+                block = this.SMALL_BUD;
+            } else if (blockState.isOf(this.SMALL_BUD) && blockState.get(AmethystClusterBlock.FACING) == direction) {
+                block = this.MEDIUM_BUD;
+            } else if (blockState.isOf(this.MEDIUM_BUD) && blockState.get(AmethystClusterBlock.FACING) == direction) {
+                block = this.LARGE_BUD;
+            } else if (blockState.isOf(this.LARGE_BUD) && blockState.get(AmethystClusterBlock.FACING) == direction) {
                 block = this.FULL_BUD;
             }
 
             if (block != null) {
-                BlockState blockState2 = block.getDefaultState().with(AmethystClusterBlock.FACING, direction).with(AmethystClusterBlock.WATERLOGGED, blockState.getFluidState().getFluid() == Fluids.WATER);
-                world.setBlockState(blockPos, blockState2);
+                BlockState toSet = block.getDefaultState().with(AmethystClusterBlock.FACING, direction).with(AmethystClusterBlock.WATERLOGGED, blockState.getFluidState().getFluid() == Fluids.WATER);
+                world.setBlockState(blockPos, toSet);
             }
 
         }
     }
 
     public List<Block> getClusters() {
-        return ImmutableList.of(SMALL_CLUSTER, MEDIUM_CLUSTER, LARGE_CLUSTER, FULL_BUD);
+        return ImmutableList.of(SMALL_BUD, MEDIUM_BUD, LARGE_BUD, FULL_BUD);
     }
 
     public List<BlockState> getClusterStates() {
