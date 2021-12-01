a95d4ad9e3b58fcb358bba4953504e1599ddf5d8
Michael Alpenfels <mwa172@uclive.ac.nz>
2021-12-01 17:26:54 +1300

    start 1.18 update

--- src/main/java/com/github/thedeathlycow/moregeodes/features/ModConfiguredFeatures.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/features/ModConfiguredFeatures.java
@@ -7,19 +7,6 @@ import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 public class ModConfiguredFeatures {
 
-
-    public static final ConfiguredFeature<?, ?> EMERALD_GEODE = ModFeatures.EMERALD_GEODE
-            .configure(ModFeatures.EMERALD_GEODE.getFeatureConfig())
-            .uniformRange(YOffset.aboveBottom(6), YOffset.fixed(46))
-            .spreadHorizontally()
-            .applyChance(53);
-
-    public static final ConfiguredFeature<?, ?> QUARTZ_GEODE = ModFeatures.QUARTZ_GEODE
-            .configure(ModFeatures.QUARTZ_GEODE.getFeatureConfig())
-            .uniformRange(YOffset.aboveBottom(12), YOffset.fixed(80))
-            .spreadHorizontally()
-            .applyChance(53);
-
     private static void register(String name, ConfiguredFeature<?, ?> configuredFeature) {
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, name, configuredFeature);
     }
