36c236fa1200137ac683ff47e2ee3e9ce4af8954
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-09 00:08:36 -1000

    revert biome modifications change; add byc support to biome tags

--- src/main/java/com/github/thedeathlycow/moregeodes/features/ModFeatures.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/features/ModFeatures.java
@@ -13,21 +13,21 @@ public class ModFeatures {
     public static void placeFeaturesInBiomes() {
         BiomeModifications.addFeature
                 (
-                        BiomeSelectors.categories(Biome.Category.MOUNTAIN).or(BiomeSelectors.tag(ModBiomeTags.HAS_EMERALD_GEODE)),
+                        BiomeSelectors.tag(ModBiomeTags.HAS_EMERALD_GEODE),
                         GenerationStep.Feature.UNDERGROUND_DECORATION,
                         Objects.requireNonNull(ModPlacedFeatures.EMERALD_GEODE.getKey().orElse(null))
                 );
 
         BiomeModifications.addFeature
                 (
-                        BiomeSelectors.foundInTheNether().or(BiomeSelectors.tag(ModBiomeTags.HAS_QUARTZ_GEODE)),
+                        BiomeSelectors.tag(ModBiomeTags.HAS_QUARTZ_GEODE),
                         GenerationStep.Feature.UNDERGROUND_DECORATION,
                         Objects.requireNonNull(ModPlacedFeatures.QUARTZ_GEODE.getKey().orElse(null))
                 );
 
         BiomeModifications.addFeature
                 (
-                        BiomeSelectors.foundInOverworld().or(BiomeSelectors.tag(ModBiomeTags.HAS_DIAMOND_GEODE)),
+                        BiomeSelectors.tag(ModBiomeTags.HAS_DIAMOND_GEODE),
                         GenerationStep.Feature.UNDERGROUND_DECORATION,
                         Objects.requireNonNull(ModPlacedFeatures.DIAMOND_GEODE.getKey().orElse(null))
                 );
