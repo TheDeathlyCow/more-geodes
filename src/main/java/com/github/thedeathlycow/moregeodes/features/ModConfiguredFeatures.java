e9f7ce64e1f74251cb213dfb8a589672595c26c5
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-13 02:50:11 -1000

    new diamond geode texture

--- src/main/java/com/github/thedeathlycow/moregeodes/features/ModConfiguredFeatures.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/features/ModConfiguredFeatures.java
@@ -11,6 +11,7 @@ import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.stateprovider.WeightedBlockStateProvider;
 
@@ -73,21 +74,28 @@ public class ModConfiguredFeatures {
                 )
         ));
 
+        BlockStateProvider middleLayer = new WeightedBlockStateProvider(
+                DataPool.<BlockState>builder()
+                        .add(Blocks.DEEPSLATE_COAL_ORE.getDefaultState(), 3)
+                        .add(Blocks.SMOOTH_BASALT.getDefaultState(), 1)
+                        .build()
+        );
+
         DIAMOND_GEODE = register("diamond_geode", new ConfiguredFeature<>(Feature.GEODE, new GeodeFeatureConfig
                 (
                         new GeodeLayerConfig
                                 (
-                                        SimpleBlockStateProvider.of(Blocks.AIR.getDefaultState()),
+                                        SimpleBlockStateProvider.of(Blocks.WATER.getDefaultState()),
                                         SimpleBlockStateProvider.of(ModBlocks.DIAMOND_GEODE.getDefaultState()),
                                         SimpleBlockStateProvider.of(ModBlocks.DIAMOND_GEODE.getDefaultState()),
-                                        SimpleBlockStateProvider.of(Blocks.DEEPSLATE_COAL_ORE.getDefaultState()),
+                                        middleLayer,
                                         SimpleBlockStateProvider.of(Blocks.SMOOTH_BASALT.getDefaultState()),
                                         List.of(ModBlocks.DIAMOND_CLUSTER.getDefaultState(), ModBlocks.DIAMOND_CLUSTER.getDefaultState()),
                                         BlockTags.FEATURES_CANNOT_REPLACE,
                                         BlockTags.GEODE_INVALID_BLOCKS
                                 ),
                         new GeodeLayerThicknessConfig(1.7D, 2.2D, 3.2D, 4.2D),
-                        new GeodeCrackConfig(0.1D, 1.0D, 2),
+                        new GeodeCrackConfig(0.0D, 1.0D, 2),
                         0.35D, 0.083D, true,
                         UniformIntProvider.create(4, 6),
                         UniformIntProvider.create(3, 4),
