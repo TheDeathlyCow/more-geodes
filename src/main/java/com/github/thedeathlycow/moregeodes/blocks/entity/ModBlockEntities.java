8cb699669ff2e5cc3b53d9388d6f681709a8fa26
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-31 01:13:09 -1000

    block entity fix

--- src/main/java/com/github/thedeathlycow/moregeodes/blocks/entity/ModBlockEntities.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/blocks/entity/ModBlockEntities.java
@@ -9,7 +9,16 @@ import net.minecraft.util.registry.Registry;
 
 public class ModBlockEntities {
 
-    public static final BlockEntityType<EchoLocatorBlockEntity> ECHO_LOCATOR = FabricBlockEntityTypeBuilder.create(EchoLocatorBlockEntity::new, ModBlocks.EMERALD_CRYSTAL_ECHO_LOCATOR).build();
+    public static final BlockEntityType<EchoLocatorBlockEntity> ECHO_LOCATOR =
+            FabricBlockEntityTypeBuilder.create(
+                            EchoLocatorBlockEntity::new,
+                            ModBlocks.EMERALD_CRYSTAL_ECHO_LOCATOR,
+                            ModBlocks.QUARTZ_CRYSTAL_ECHO_LOCATOR,
+                            ModBlocks.AMETHYST_CRYSTAL_ECHO_LOCATOR,
+                            ModBlocks.DIAMOND_CRYSTAL_ECHO_LOCATOR,
+                            ModBlocks.ECHO_CRYSTAL_ECHO_LOCATOR
+                    )
+                    .build();
 
     public static void registerBlockEntities() {
         register("echo_locator", ECHO_LOCATOR);
