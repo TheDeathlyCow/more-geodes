3ebb4f2c38119f256b4248335d285e28262991b8
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-08-06 21:52:42 -1000

    custom sounds for echo geodes

--- src/main/java/com/github/thedeathlycow/moregeodes/sounds/GeodesSoundEvents.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/sounds/GeodesSoundEvents.java
@@ -8,9 +8,13 @@ import net.minecraft.util.registry.Registry;
 public class GeodesSoundEvents {
 
     public static final SoundEvent CRYSTAL_RESONATE = GeodesSoundEvents.of("block.geodes.echo_locator.resonate");
+    public static final SoundEvent BLOCK_ECHO_BLOCK_BREAK = GeodesSoundEvents.of("block.geodes.echo_block.break");
+    public static final SoundEvent BLOCK_ECHO_BLOCK_PLACE = GeodesSoundEvents.of("block.geodes.echo_block.place");
 
     public static void registerSoundEvents() {
         register(CRYSTAL_RESONATE);
+        register(BLOCK_ECHO_BLOCK_BREAK);
+        register(BLOCK_ECHO_BLOCK_PLACE);
     }
 
     private static void register(SoundEvent event) {
