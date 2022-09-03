df75fd0a725817879030f43b4e3eb3231b712ba7
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-09-03 00:22:07 -1000

    Revert "simple framed openable door"
    This reverts commit 16cbdef898bd04cf6722f2e5ce474b1db6b3ff3d.

--- src/main/java/com/github/thedeathlycow/moregeodes/MoreGeodes.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/MoreGeodes.java
@@ -2,7 +2,6 @@ package com.github.thedeathlycow.moregeodes;
 
 import com.github.thedeathlycow.moregeodes.blocks.ModBlocks;
 import com.github.thedeathlycow.moregeodes.blocks.entity.ModBlockEntities;
-import com.github.thedeathlycow.moregeodes.config.GeodesConfig;
 import com.github.thedeathlycow.moregeodes.features.ModFeatures;
 import com.github.thedeathlycow.moregeodes.item.ModItems;
 import com.github.thedeathlycow.moregeodes.sounds.GeodesSoundEvents;
@@ -16,12 +15,9 @@ public class MoreGeodes implements ModInitializer {
     public static final String MODID = "geodes";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public static final GeodesConfig CONFIG = new GeodesConfig();
-
     @Override
     public void onInitialize() {
         LOGGER.info("Beginning More Geodes initialization");
-        CONFIG.loadConfig();
         ModBlocks.registerBlocks();
         ModItems.registerItems();
         ModFeatures.placeFeaturesInBiomes();
--- src/main/java/com/github/thedeathlycow/moregeodes/mixin/geode/GeodeConfigImpl.java
+++
@@ -1,8 +0,0 @@
-package com.github.thedeathlycow.moregeodes.mixin.geode;
-
-import net.minecraft.world.gen.feature.GeodeFeature;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(GeodeFeature.class)
-public class GeodeConfigImpl {
-}
