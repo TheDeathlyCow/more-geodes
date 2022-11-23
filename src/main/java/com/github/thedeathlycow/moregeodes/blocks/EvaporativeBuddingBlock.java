2d35b266d476eeba93a369711e415054aa088e7d
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-11-22 18:48:53 -1000

    only evaporate with sun light

--- src/main/java/com/github/thedeathlycow/moregeodes/blocks/EvaporativeBuddingBlock.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/blocks/EvaporativeBuddingBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.LightType;
 import net.minecraft.world.WorldView;
 
 import java.util.List;
@@ -51,7 +52,7 @@ public class EvaporativeBuddingBlock extends GeodeBuddingBlock {
                         Block.NOTIFY_LISTENERS
                 );
             }
-        } else {
+        } else if (world.getLightLevel(LightType.SKY, pos.up()) > 0) {
             // decrease moisture
             if (currentMoisture > 0) {
                 world.setBlockState(
