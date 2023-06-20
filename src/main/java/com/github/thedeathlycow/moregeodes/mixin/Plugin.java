0d5a863ad70512dfd1a22d7e12fa92502eac11ae
TheDeathlyCow <thedeathlycow@gmail.com>
2023-06-21 00:32:07 +1200

    better local variable names and set a release version

--- src/main/java/com/github/thedeathlycow/moregeodes/mixin/Plugin.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/mixin/Plugin.java
@@ -12,6 +12,7 @@ import java.util.Set;
 public class Plugin implements IMixinConfigPlugin {
     @Override
     public void onLoad(String mixinPackage) {
+        // do nothing
     }
 
     @Override
@@ -21,22 +22,31 @@ public class Plugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, @NotNull String mixinClassName) {
-        String l = Plugin.class.getPackageName();
-        if (!mixinClassName.startsWith(l)) return true;
-        int i = l.split("\\.").length;
-        String[] s = mixinClassName.split("\\.");
-        if (s.length > i && s[i].equalsIgnoreCase("compat")) {
-            return switch (s[i+1]) {
+        String thisPackageName = Plugin.class.getPackageName();
+
+        if (!mixinClassName.startsWith(thisPackageName)) {
+            return true;
+        }
+
+        int numPackages = thisPackageName.split("\\.").length;
+        String[] splitMixinClassName = mixinClassName.split("\\.");
+
+        boolean isCompatClass = splitMixinClassName.length > numPackages
+                && splitMixinClassName[numPackages].equalsIgnoreCase("compat");
+
+        if (isCompatClass) {
+            return switch (splitMixinClassName[numPackages + 1]) {
                 case "carpet" -> FabricLoader.getInstance().isModLoaded("carpet");
                 default -> true;
             };
         }
+
         return true;
     }
 
     @Override
     public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-
+        // do nothing
     }
 
     @Override
@@ -46,11 +56,11 @@ public class Plugin implements IMixinConfigPlugin {
 
     @Override
     public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
+        // do nothing
     }
 
     @Override
     public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
+        // do nothing
     }
 }
