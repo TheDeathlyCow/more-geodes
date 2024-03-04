c0756fe1226203cb6218ad677be0962cf747ef0f
TheDeathlyCow <thedeathlycow@gmail.com>
2024-03-03 16:56:32 -1000

    slight reduce echo locator block range, remove entity glow

--- src/main/java/com/github/thedeathlycow/moregeodes/item/EchoLocatorItem.java
+++ src/main/java/com/github/thedeathlycow/moregeodes/item/EchoLocatorItem.java
@@ -17,37 +17,12 @@ import org.jetbrains.annotations.Nullable;
 
 public class EchoLocatorItem extends CrystalLocatorItem {
 
-    public static final int BLOCK_RANGE = 8;
-
-    public static final int ENTITY_RANGE = 48;
+    public static final int BLOCK_RANGE = 6;
 
     public static final int ECHO_LOCATOR_COOL_DOWN = CrystalLocatorItem.CRYSTAL_LOCATOR_COOL_DOWN * 4;
 
-    public static final int GLOW_TIME_SECONDS = 60;
-
     public EchoLocatorItem(Settings settings, int range, int coolDown) {
         super(settings, range, coolDown);
-    }
-
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (!world.isClient) {
-            var box = new Box(user.getBlockPos())
-                    .expand(ENTITY_RANGE);
-
-            world.getNonSpectatingEntities(LivingEntity.class, box)
-                    .forEach(
-                            livingEntity -> {
-                                if (livingEntity != user) {
-                                    livingEntity.addStatusEffect(
-                                            new StatusEffectInstance(StatusEffects.GLOWING, GLOW_TIME_SECONDS)
-                                    );
-                                }
-                            }
-                    );
-        }
-
-        return super.use(world, user, hand);
     }
 
     @Override
